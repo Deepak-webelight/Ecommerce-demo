e05afd8d539a703a03698b17eaf1b8b3127ada06
Deepak-webelight <deepakkumarm@webelight.co.in>
2024-04-16 10:34:51 +0530

    removed .env from github

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,24 @@
 import express, { Request, Response, Express } from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth";
+import connectDb from "./config/connectDb";
+import envProvider from "./utils/envProvider";
 
 const app: Express = express();
-
 app.use(express.json());
 app.use(cors());
 app.use("/auth", authRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Hello from gateway!");
 });
 
-export default app;
+connectDb()
+  .then(() => {
+    app.listen(envProvider.PORT, () => {
+      console.log(
+        "Server is fire at " + `http://localhost:${envProvider.PORT} 🚀`
+      );
+    });
+  })
+  .catch((err) => console.error(err));
