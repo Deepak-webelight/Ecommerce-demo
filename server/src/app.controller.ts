2175f4da68cc353a01df671b92a5398894e7d90c
Deepak-webelight <deepakkumarm@webelight.co.in>
2024-04-29 11:21:20 +0530

    fixed Github Comments

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,10 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-// import { PublicRoute } from './guards/auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // @PublicRoute()
   @Get()
   getHello(): string {
     return this.appService.getHello();
